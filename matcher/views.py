c0fdf6d0ed7b4efb075fb58f9486ff56788a6d17
Jéssica Silva <jessica.akyla@gmail.com>
2025-05-26 11:44:43 -0300

    feat: Add function to assign agents to tickets

--- matcher/views.py
+++ matcher/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from api.models import Agent, TicketModel
+from django.db.models import Count, Q
 
-# Create your views here.
+class TransferTicketAgent(): 
+     
+    def assign_agent_ticket(self):
+        
+        return Agent.objects.filter(profile= Agent.PROFILE_ATTENDANT, status= Agent.STATUS_ONLINE).annotate(
+            count_agent_ticket=Count('tickets', filter=Q(tickets__status='Open'))
+        ).order_by('count_agent_ticket').first()
+        
+                   
